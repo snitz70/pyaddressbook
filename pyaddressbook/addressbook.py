a23b6a5baa72b2bdaf56dd73d7bbe13fe6f29820
Brian Snyder <bsnyder@technology4retailers.com>
2016-05-18 16:30:45 -0500

    added table for Contact

--- pyaddressbook/addressbook.py
+++ pyaddressbook/addressbook.py
@@ -13,6 +13,19 @@ class Addressbook(BaseModel):
     name = pw.CharField(unique=True)
 
 
+class Contact(BaseModel):
+    name = pw.CharField()
+    address = pw.CharField()
+    city = pw.CharField()
+    state = pw.CharField()
+    zipcode = pw.CharField()
+    addressbook = pw.ForeignKeyField(Addressbook)
+
+
+def create_tables():
+    database.create_tables([Addressbook, Contact], True)
+
+
 def create_addressbook(name):
     try:
         with database.transaction():
